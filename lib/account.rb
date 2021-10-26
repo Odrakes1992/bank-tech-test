53d0498f6ce9f530a8665fcc0f7f9aca7fdeda70
Osneil Drakes <osneil-drakes@hotmail.com>
2021-10-26 15:31:09 +0100

    OD add and pass test for adding and depositing monies to bank account

--- lib/account.rb
+++ lib/account.rb
@@ -4,9 +4,17 @@ class Account
     @balance = 0 
   end 
 
-def show_balance
-  @balance
-end 
+  def show_balance
+    @balance
+  end 
 
+
+  def deposit(monies)
+    @balance += monies
+  end
+
+  def withdraw(monies)
+    @balance -= monies
+  end 
 
 end 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,24 @@ describe Account do
     expect(subject.show_balance).to eq 0 
   end 
 
+  it 'allows account holder to desposit monies' do 
+    subject.deposit(100)
+    expect(subject.show_balance).to eq 100
+  end 
+
+  it 'allows account holder to withdraw funds' do 
+    subject.deposit(1000)
+    subject.withdraw(500)
+    expect(subject.show_balance).to eq 500
+  end 
+
+
+  describe 'test for use cases / errors ' do 
+    it 'should accept string as entries' do 
+
+    end 
+
+    it 'should show error if withdrawal/deposit is not numerical string or integer'
+  end 
+
 end 
